b322dde3b68a526f920600faac58dfb6972a0f70
isaacquinapallo <151254893+isaacquinapallo@users.noreply.github.com>
2025-05-15 16:48:33 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "Mis Credenciales",
+  authDomain: "Mis Credenciales",
+  projectId: "Mis Credenciales",
+  storageBucket: "Mis Credenciales",
+  messagingSenderId: "Mis Credenciales",
+  appId: "Mis Credenciales",
+  measurementId: "Mis Credenciales"
+  }
+};
